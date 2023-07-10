4964aa8a02e42213e749f28f6d68df94fe9f0a38
ayush-ahire <ayushgithub8@gmail.com>
2023-07-10 16:47:29 +0530

    metaversus

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,13 +1,12 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div variants={fadeIn('right', 'spring', index * 0.5, 1)}
     className={`relative ${active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
-      } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
     <img src={imgUrl} alt={title} className="absolute w-full h-full rounded-[24px] object-cover" />
